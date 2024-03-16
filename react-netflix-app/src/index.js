5d2422adff8e322e990950f0f026801392d845f1
Leeyanggoo <didrn94553@gmail.com>
2024-03-16 14:49:44 +0900

    asd

--- react-netflix-app/src/index.js
+++ react-netflix-app/src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="learn-REACT-by-following/react-netflix-app">
     <App />
   </BrowserRouter>
 );
